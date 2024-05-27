617c29cb7cedeeadf954efeccbcf3b5f7efec4d1
TiepHoang23 <89854924+TiepHoang23@users.noreply.github.com>
2024-05-27 22:50:24 +0700

    color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "",
-  mode: "jit",
+  prefix: '',
+  mode: 'jit',
 
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
 
   theme: {
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "940px",
-      xl: "1100px",
-      "2xl": "1300px",
+      sm: '640px',
+      md: '768px',
+      lg: '940px',
+      xl: '1100px',
+      '2xl': '1300px',
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#FFC49B',
+          100: '#F86E4C',
+          200: '#D04C2E',
+          300: '#A92811',
+          400: '#820000',
+          500: '#5F0000',
+        },
+        secondary: {
+          100: '#050505',
+        },
+      },
     },
   },
 };
